7b8c30272975303c2fc676e4d9738c0f87ff39d6
y-komori <komo@littleforest.jp>
2007-11-02 07:29:38 +0000

    ・未使用のメッセージコードを削除した.

--- uruma/src/main/java/org/seasar/uruma/core/UrumaMessageCodes.java
+++ uruma/src/main/java/org/seasar/uruma/core/UrumaMessageCodes.java
@@ -53,12 +53,12 @@ public interface UrumaMessageCodes {
     /**
      * 未サポートのクラスが指定された場合のメッセージコードです。
      */
-    public static final String UNSUPPORTED_CLASS = "EURM0005";
+    public static final String UNSUPPORTED_CLASS = "EURM0004";
 
     /**
      * 型が想定している型に一致しない場合のメッセージコードです。
      */
-    public static final String TYPE_MISS_MATCH = "EURM0006";
+    public static final String TYPE_MISS_MATCH = "EURM0005";
 
     // -------------------------------------------------------------------------
     // XMLパース関連メッセージコード (01xx)
@@ -66,62 +66,62 @@ public interface UrumaMessageCodes {
     /**
      * レイアウトが見つからない場合のメッセージコードです。
      */
-    public static final String LAYOUT_NOTO_FOUND = "EURM0101";
+    public static final String LAYOUT_NOTO_FOUND = "EURM0100";
 
     /**
      * レイアウトデータが見つからない場合のメッセージコードです。
      */
-    public static final String LAYOUT_DATA_NOT_FOUND = "EURM0102";
+    public static final String LAYOUT_DATA_NOT_FOUND = "EURM0101";
 
     /**
      * レンダラが見つからない場合のメッセージコードです。
      */
-    public static final String RENDERER_NOT_FOUND = "EURM0103";
+    public static final String RENDERER_NOT_FOUND = "EURM0102";
 
     /**
      * イベントリスナが見つからない場合のメッセージコードです。
      */
-    public static final String SWT_EVENT_LISTENER_NOT_FOUND = "EURM0104";
+    public static final String SWT_EVENT_LISTENER_NOT_FOUND = "EURM0103";
 
     /**
      * アノテーションで指定された id が画面定義 XML 上に存在しないことを示すメッセージコードです。
      */
-    public static final String WIDGET_NOT_FOUND_ON_XML = "EURM0105";
+    public static final String WIDGET_NOT_FOUND_ON_XML = "EURM0104";
 
     /**
      * コンポーネント ID が重複して定義されている際のメッセージコードです。
      */
-    public static final String DUPLICATE_COMPONENT_ID = "EURM0106";
+    public static final String DUPLICATE_COMPONENT_ID = "EURM0105";
 
     /**
      * 継承先コンポーネントが見つからない場合のメッセージコードです。
      */
-    public static final String EXTEND_TARGET_COMPONENT_NOT_FOUND = "EURM0107";
+    public static final String EXTEND_TARGET_COMPONENT_NOT_FOUND = "EURM0106";
 
     /**
      * 継承先プロパティが見つからない場合のメッセージコードです。
      */
-    public static final String EXTEND_TARGET_PROPERTY_NOT_FOUND = "EURM0108";
+    public static final String EXTEND_TARGET_PROPERTY_NOT_FOUND = "EURM0107";
 
     /**
      * {@link UIComponent} が見つからない場合のメッセージコードです。
      */
-    public static final String UICOMPONENT_NOT_FOUND = "EURM0109";
+    public static final String UICOMPONENT_NOT_FOUND = "EURM0108";
 
     /**
      * {@link ValueBinder} が見つからない場合のメッセージコードです。
      */
-    public static final String VALUE_BINDER_NOT_FOUND = "EURM0111";
+    public static final String VALUE_BINDER_NOT_FOUND = "EURM0109";
 
     /**
      * 画面定義テンプレートをファイルから読み込む際のメッセージコードです。
      */
-    public static final String LOAD_TEMPLATE_FROM_FILE = "IURM0112";
+    public static final String LOAD_TEMPLATE_FROM_FILE = "IURM0110";
 
     /**
      * 画面定義テンプレートをキャッシュから読み込む際のメッセージコードです。
      */
-    public static final String LOAD_TEMPLATE_FROM_CACHE = "IURM0113";
+    public static final String LOAD_TEMPLATE_FROM_CACHE = "IURM0111";
 
     // -------------------------------------------------------------------------
     // バインディング関連メッセージコード (02xx)
@@ -129,72 +129,72 @@ public interface UrumaMessageCodes {
     /**
      * {@link InitializeMethod} アノテートされたメソッドが複数存在する場合のエラーコードです。
      */
-    public static final String DUPLICATE_INITIALIZE_METHOD = "EURM0204";
+    public static final String DUPLICATE_INITIALIZE_METHOD = "EURM0200";
 
     /**
      * イニシャライズメソッドが引数・戻り値なしのメソッドでない場合のエラーコードです。
      */
-    public static final String INVALID_INITIALIZE_METHOD = "EURM0205";
+    public static final String INVALID_INITIALIZE_METHOD = "EURM0201";
 
     /**
      * イニシャライズメソッド実行中に例外が発生した場合のエラーコードです。
      */
-    public static final String EXCEPTION_ON_INVOKING_INITIALIZE_METHOD = "EURM0206";
+    public static final String EXCEPTION_ON_INVOKING_INITIALIZE_METHOD = "EURM0202";
 
     /**
      * サポートされていない型のウィジットに対してバインディングしようとした場合のメッセージコードです。
      */
-    public static final String WIDGET_NOT_SUPPORTED = "EURM0207";
+    public static final String WIDGET_NOT_SUPPORTED = "EURM0203";
 
     /**
      * アノテートされたフィールドに対応するウィジットが存在しない場合のメッセージコードです。
      */
-    public static final String WIDGET_NOT_FOUND = "EURM0208";
+    public static final String WIDGET_NOT_FOUND = "EURM0204";
 
     /**
      * EnablesDepending でターゲットウィジットがサポートされていない際のメッセージコードです。<br />
      */
-    public static final String DEPENDING_WIDGET_NOT_SUPPORTED = "EJFC0211";
+    public static final String DEPENDING_WIDGET_NOT_SUPPORTED = "EJFC0205";
 
     /**
      * EnablesDepending でターゲットに対する選択条件がサポートされていない際のメッセージコードです。<br />
      */
-    public static final String DEPENDING_TYPE_NOT_SUPPORTED = "EJFC0212";
+    public static final String DEPENDING_TYPE_NOT_SUPPORTED = "EJFC0206";
 
     /**
      * バインド先とバインド元の型が一致しない場合のメッセージコードです。
      */
-    public static final String CLASS_NOT_MUTCH = "EURM0214";
+    public static final String CLASS_NOT_MUTCH = "EURM0207";
 
     /**
      * バインド処理を実行する際のメッセージコードです。<br />
      */
-    public static final String DO_BINDING = "DURM0215";
+    public static final String DO_BINDING = "DURM0208";
 
     /**
      * {@link ApplicationContext} からオブジェクトへ値をバインドする際のメッセージコードです。
      */
-    public static final String IMPORT_APPLICATION_CONTEXT = "DURM0216";
+    public static final String IMPORT_APPLICATION_CONTEXT = "DURM0209";
 
     /**
      * オブジェクトから {@link ApplicationContext} へ値をバインドする際のメッセージコードです。
      */
-    public static final String EXPORT_APPLICATION_CONTEXT = "DURM0217";
+    public static final String EXPORT_APPLICATION_CONTEXT = "DURM0210";
 
     /**
      * メソッドバインディング開始時のメッセージコードです。
      */
-    public static final String START_METHOD_CALL = "IURM0218";
+    public static final String START_METHOD_CALL = "IURM0211";
 
     /**
      * メソッドバインディング終了時のメッセージコードです。
      */
-    public static final String END_METHOD_CALL = "IURM0219";
+    public static final String END_METHOD_CALL = "IURM0212";
 
     /**
      * EnablesDepending ターゲットが見つからない場合のメッセージコードです。
      */
-    public static final String ENABLES_DEPENDING_TARGET_NOT_FOUND = "EURM0220";
+    public static final String ENABLES_DEPENDING_TARGET_NOT_FOUND = "EURM0213";
 
     // -------------------------------------------------------------------------
     // レンダリング関連メッセージコード (03xx)
@@ -207,17 +207,17 @@ public interface UrumaMessageCodes {
     /**
      * レンダリング時の値の設定に失敗した場合のメッセージコードです。
      */
-    public static final String RENDER_MAPPING_FAILED = "EURM0302";
+    public static final String RENDER_MAPPING_FAILED = "EURM0301";
 
     /**
      * レンダリング時に実際の型が想定していた型と異なっていた場合のエラーコードです。
      */
-    public static final String UNSUPPORTED_TYPE_ERROR = "EURM0303";
+    public static final String UNSUPPORTED_TYPE_ERROR = "EURM0302";
 
     /**
      * ViewPart のレンダリング時にルートコンポーネントが {@link ViewPartComponent} でなかった場合のエラーコードです。
      */
-    public static final String REQUIRED_VIEWPART_ERROR = "EJFC0304";
+    public static final String REQUIRED_VIEWPART_ERROR = "EJFC0303";
 
     // -------------------------------------------------------------------------
     // ウィンドウ管理関連メッセージコード (04xx)
@@ -225,33 +225,33 @@ public interface UrumaMessageCodes {
     /**
      * ウィンドウオープン時のメッセージコードです。
      */
-    public static final String OPEN_WINDOW = "IURM0401";
+    public static final String OPEN_WINDOW = "IURM0400";
 
     /**
      * ウィンドウ初期化時のメッセージコードです。
      */
-    public static final String INIT_WINDOW = "IURM0402";
+    public static final String INIT_WINDOW = "IURM0401";
 
     /**
      * ウィンドウクローズ時のメッセージコードです。
      */
-    public static final String CLOSE_WINDOW = "IURM0403";
+    public static final String CLOSE_WINDOW = "IURM0402";
 
     // -------------------------------------------------------------------------
     // システムデバッグログ関連メッセージコード (99xx)
     // -------------------------------------------------------------------------
     /**
-     * {@link StandAloneUrumaStarter} 初期化時のメッセージコードです。<br />
+     * {@link StandAloneUrumaStarter} 初期化時のメッセージコードです。
      */
     public static final String STAND_ALONE_URUMA_STARTER_INIT = "DURM9900";
 
     /**
-     * イメージバンドルファイル読み込み時のメッセージコードです。<br />
+     * イメージバンドルファイル読み込み時のメッセージコードです。
      */
     public static final String LOADING_IMAGE_BUNDLE = "DURM9901";
 
     /**
-     * {@link StandAloneUrumaStarter} 終了時のメッセージコードです。<br />
+     * {@link StandAloneUrumaStarter} 終了時のメッセージコードです。
      */
     public static final String STAND_ALONE_URUMA_STARTER_STOP = "DURM9902";
 }
