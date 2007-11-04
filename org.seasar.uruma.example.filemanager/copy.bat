578d4a12bd3be95cd17f92f64bee93c43d04a9e6
y-komori <komo@littleforest.jp>
2007-11-04 13:17:30 +0000

    ・copy.bat は不要になったので削除.

--- org.seasar.uruma.example.filemanager/copy.bat
+++
@@ -1,9 +0,0 @@
-D:
-cd \develop\uruma
-call mvn clean install
-
-rem copy C:\.m2\repository\org\seasar\container\s2-framework\2.4.14-SNAPSHOT\s2-framework-2.4.14-SNAPSHOT.jar D:\Develop\org.seasar.rcp.example.filemanager\lib
-copy C:\.m2\repository\org\seasar\uruma\uruma\0.2.0-SNAPSHOT\uruma-0.2.0-SNAPSHOT.jar D:\Develop\org.seasar.uruma.example.filemanager\lib
-copy C:\.m2\repository\org\seasar\uruma\uruma\0.2.0-SNAPSHOT\uruma-0.2.0-SNAPSHOT-sources.jar D:\Develop\org.seasar.uruma.example.filemanager\lib
-
-pause
