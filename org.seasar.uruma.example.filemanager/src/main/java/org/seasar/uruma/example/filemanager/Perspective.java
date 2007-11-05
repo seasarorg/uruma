cb4029925f807bd4608b6619f14f81a155bf5ba3
y-komori <komo@littleforest.jp>
2007-11-05 14:35:04 +0000

    ・ViewPart動的追加作業中.

--- org.seasar.uruma.example.filemanager/src/main/java/org/seasar/uruma/example/filemanager/Perspective.java
+++ org.seasar.uruma.example.filemanager/src/main/java/org/seasar/uruma/example/filemanager/Perspective.java
@@ -5,13 +5,15 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
 
-	public void createInitialLayout(IPageLayout layout) {
+	public void createInitialLayout(final IPageLayout layout) {
 		layout.setEditorAreaVisible(false);
 
-		layout.addStandaloneView("org.seasar.rcp.example.fileman.FolderView",
-				true, IPageLayout.LEFT, 0.3f, layout.getEditorArea());
+		layout.addStandaloneView(
+				"org.seasar.uruma.example.filemanager.folderView", true,
+				IPageLayout.LEFT, 0.3f, layout.getEditorArea());
 
-		layout.addStandaloneView("org.seasar.rcp.example.fileman.FileView",
-				true, IPageLayout.RIGHT, 0.7f, layout.getEditorArea());
+		layout.addStandaloneView(
+				"org.seasar.uruma.example.filemanager.fileView", true,
+				IPageLayout.RIGHT, 0.7f, layout.getEditorArea());
 	}
 }
