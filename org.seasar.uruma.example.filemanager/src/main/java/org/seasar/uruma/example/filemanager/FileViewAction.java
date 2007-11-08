28a8b371be76191c817bbadf09bdc4de19451635
y-komori <komo@littleforest.jp>
2007-11-08 11:14:05 +0000

    ・ファイルをダブルクリックで実行できるようにした.

--- org.seasar.uruma.example.filemanager/src/main/java/org/seasar/uruma/example/filemanager/FileViewAction.java
+++ org.seasar.uruma.example.filemanager/src/main/java/org/seasar/uruma/example/filemanager/FileViewAction.java
@@ -6,8 +6,13 @@ import java.util.Date;
 import java.util.Formatter;
 import java.util.List;
 
+import org.eclipse.swt.program.Program;
+import org.seasar.framework.util.StringUtil;
+import org.seasar.uruma.annotation.EventListener;
+import org.seasar.uruma.annotation.EventListenerType;
 import org.seasar.uruma.annotation.ExportValue;
 import org.seasar.uruma.annotation.Form;
+import org.seasar.uruma.annotation.ImportSelection;
 import org.seasar.uruma.annotation.SelectionListener;
 
 /**
@@ -18,6 +23,9 @@ import org.seasar.uruma.annotation.SelectionListener;
 public class FileViewAction {
 	@ExportValue(id = "fileDetailTable")
 	public List<FileDto> fileList = new ArrayList<FileDto>();
+
+	@ImportSelection(id = "fileDetailTable")
+	public List<FileDto> selectedFile;
 
 	@SelectionListener
 	public void selectionChanged(final File parentFolder) {
@@ -32,7 +40,9 @@ public class FileViewAction {
 			fileDto.fileName = file.getName();
 
 			if (file.isFile()) {
-				fileDto.fileSize = Long.toString(file.length());
+				Formatter formatter = new Formatter();
+				fileDto.fileSize = formatter.format("%,d", file.length()).out()
+						.toString();
 			} else {
 				fileDto.fileSize = "";
 			}
@@ -43,6 +53,21 @@ public class FileViewAction {
 			fileDto.fileUpdateTime = formatter.out().toString();
 
 			fileList.add(fileDto);
+		}
+	}
+
+	@EventListener(id = "fileDetailTable", type = EventListenerType.MOUSE_DOUBLE_CLICK)
+	public void onSelected() {
+		if (selectedFile.size() == 1) {
+			FileDto dto = selectedFile.get(0);
+			File file = new File(dto.absolutePath);
+			if (file.isFile()) {
+				Program program = Program.findProgram(StringUtil
+						.substringToLast(dto.fileName, "."));
+				if (program != null) {
+					Program.launch(dto.absolutePath);
+				}
+			}
 		}
 	}
 }
