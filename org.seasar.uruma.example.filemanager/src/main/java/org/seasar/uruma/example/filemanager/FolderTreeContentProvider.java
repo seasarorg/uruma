f09ea2dd21c7c85044a4e6d3e716f7d51279448a
y-komori <komo@littleforest.jp>
2007-11-08 15:02:23 +0000

    ・Copyright 追加.

--- org.seasar.uruma.example.filemanager/src/main/java/org/seasar/uruma/example/filemanager/FolderTreeContentProvider.java
+++ org.seasar.uruma.example.filemanager/src/main/java/org/seasar/uruma/example/filemanager/FolderTreeContentProvider.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.uruma.example.filemanager;
 
 import java.io.File;
@@ -9,7 +24,7 @@ import org.eclipse.jface.viewers.Viewer;
 
 public class FolderTreeContentProvider implements ITreeContentProvider {
 
-	public Object[] getChildren(Object parentElement) {
+	public Object[] getChildren(final Object parentElement) {
 		File parentFolder = (File) parentElement;
 		File[] children = parentFolder.listFiles();
 		List<File> temp = new ArrayList<File>();
@@ -23,23 +38,24 @@ public class FolderTreeContentProvider implements ITreeContentProvider {
 		return temp.toArray(new File[temp.size()]);
 	}
 
-	public Object getParent(Object element) {
+	public Object getParent(final Object element) {
 		File file = (File) element;
 		return file.getParentFile();
 	}
 
-	public boolean hasChildren(Object element) {
+	public boolean hasChildren(final Object element) {
 		File file = (File) element;
 		return file.isDirectory();
 	}
 
-	public Object[] getElements(Object inputElement) {
+	public Object[] getElements(final Object inputElement) {
 		return getChildren(inputElement);
 	}
 
 	public void dispose() {
 	}
 
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+	public void inputChanged(final Viewer viewer, final Object oldInput,
+			final Object newInput) {
 	}
 }
--- org.seasar.uruma.example.filemanager/src/main/java/org/seasar/uruma/example/filemanager/FolderTreeLabelProvider.java
+++ org.seasar.uruma.example.filemanager/src/main/java/org/seasar/uruma/example/filemanager/FolderTreeLabelProvider.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.uruma.example.filemanager;
 
 import java.io.File;
@@ -9,13 +24,13 @@ import org.seasar.eclipse.common.util.ImageManager;
 public class FolderTreeLabelProvider extends LabelProvider {
 
 	@Override
-	public Image getImage(Object element) {
+	public Image getImage(final Object element) {
 		Image folderImage = ImageManager.loadImage("folder");
 		return folderImage;
 	}
 
 	@Override
-	public String getText(Object element) {
+	public String getText(final Object element) {
 		File folder = (File) element;
 		return folder.getName();
 	}
