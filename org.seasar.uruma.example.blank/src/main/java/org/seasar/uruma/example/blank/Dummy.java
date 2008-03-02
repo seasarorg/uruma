1ca76ef759b8daab2269591ea5d5b70b011aaabf
y-komori <komo@littleforest.jp>
2008-03-02 01:51:49 +0000

    ・ダミークラスを追加.

---
+++ org.seasar.uruma.example.blank/src/main/java/org/seasar/uruma/example/blank/Dummy.java
@@ -0,0 +1,10 @@
+package org.seasar.uruma.example.blank;
+
+/**
+ * Uruma アプリケーションのためのダミークラスです。<br />
+ * Uruma アプリケーションでは最低一つの Java クラスがクラスパス内に存在する必要があります。<br />
+ * 他のクラスを作成した場合、本クラスは削除して構いません。
+ */
+public class Dummy {
+
+}
