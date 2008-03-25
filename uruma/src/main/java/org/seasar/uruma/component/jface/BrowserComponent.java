a2c0b8dabd2a63cf69851158328099a845efc328
y-komori <komo@littleforest.jp>
2008-03-25 12:44:04 +0000

    ・BrowserComponent を public フィールド化した.

--- uruma/src/main/java/org/seasar/uruma/component/jface/BrowserComponent.java
+++ uruma/src/main/java/org/seasar/uruma/component/jface/BrowserComponent.java
@@ -29,117 +29,21 @@ public class BrowserComponent extends ControlComponent {
 
     @RenderingPolicy(conversionType = ConversionType.TEXT)
     @FieldDescription("テキスト")
-    private String text;
+    public String text;
 
     @RenderingPolicy(conversionType = ConversionType.TEXT)
     @FieldDescription("URL")
-    private String url;
+    public String url;
 
     @RenderingPolicy(conversionType = ConversionType.BOOLEAN)
     @FieldDescription("前ぺージに戻るの許可状態")
-    private String backEnabled;
+    public String backEnabled;
 
     @RenderingPolicy(conversionType = ConversionType.BOOLEAN)
     @FieldDescription("次ぺージに戻るの許可状態")
-    private String forwardEnabled;
+    public String forwardEnabled;
 
     @RenderingPolicy(conversionType = ConversionType.BOOLEAN)
     @FieldDescription("フォーカスの許可状態")
-    private String focusControl;
-
-    /**
-     * テキストを取得します。<br />
-     * 
-     * @return テキスト
-     */
-    public String getText() {
-        return this.text;
-    }
-
-    /**
-     * テキストを設定します。<br />
-     * 
-     * @param text
-     *            テキスト
-     */
-    public void setText(final String text) {
-        this.text = text;
-    }
-
-    /**
-     * URLを取得します。<br />
-     * 
-     * @return テキスト
-     */
-    public String getUrl() {
-        return this.url;
-    }
-
-    /**
-     * URLを設定します。<br />
-     * 
-     * @param text
-     *            テキスト
-     */
-    public void setUrl(final String url) {
-        this.url = url;
-    }
-
-    /**
-     * 前ぺージに戻るの許可状態を取得します。<br />
-     * 
-     * @return テキスト
-     */
-    public String getBackEnabled() {
-        return this.backEnabled;
-    }
-
-    /**
-     * 前ぺージに戻るの許可状態を設定します。<br />
-     * 
-     * @param text
-     *            テキスト
-     */
-    public void setBackEnabled(final String backEnabled) {
-        this.backEnabled = backEnabled;
-    }
-
-    /**
-     * フォーカスの許可状態を取得します。<br />
-     * 
-     * @return テキスト
-     */
-    public String getFocusControl() {
-        return this.focusControl;
-    }
-
-    /**
-     * フォーカスの許可状態を設定します。<br />
-     * 
-     * @param text
-     *            テキスト
-     */
-    public void setFocusControl(final String focusControl) {
-        this.focusControl = focusControl;
-    }
-
-    /**
-     * 次ぺージに戻るの許可状態を取得します。<br />
-     * 
-     * @return テキスト
-     */
-    public String getForwardEnabled() {
-        return this.forwardEnabled;
-    }
-
-    /**
-     * 次ぺージに戻るの許可状態を設定します。<br />
-     * 
-     * @param text
-     *            テキスト
-     */
-    public void setForwardEnabled(final String forwardEnabled) {
-        this.forwardEnabled = forwardEnabled;
-    }
-
+    public String focusControl;
 }
