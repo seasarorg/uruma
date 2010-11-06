c6235ecbd3ddf3e56ccd67d7c49056d98816662c
y-komori <komo@littleforest.jp>
2010-11-06 16:08:15 +0000

    ・uruma-eclipseプラグインでクラスパスを設定するようにした。

--- uruma-example-janken/src/main/java/org/seasar/uruma/example/janken/JankenAction.java
+++ uruma-example-janken/src/main/java/org/seasar/uruma/example/janken/JankenAction.java
@@ -21,7 +21,6 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
-import org.seasar.eclipse.common.util.ImageManager;
 import org.seasar.eclipse.common.util.SWTUtil;
 import org.seasar.framework.container.annotation.tiger.Component;
 import org.seasar.uruma.annotation.EventListener;
